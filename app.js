34ed0b156d484d918f3a6094004a930665ca1e1c
sprusr <scottystreet@gmail.com>
2016-10-22 20:08:35 +0100

    Add NLP functionality via LUIS

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var builder = require('botbuilder');
+var algoliasearch = require('algoliasearch');
+var secrets = require('./secrets.js');
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
@@ -7,31 +9,58 @@ var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v
 
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 
+var client = algoliasearch(secrets.algolia.applicationID, secrets.algolia.apiKey);
+var index = client.initIndex('stations');
+
 bot.dialog('/', dialog);
 
 dialog.matches('Update station status', [
-  function (session, args, next) {
-    console.log(args);
-    next();
-  },
+  searchFor,
   function (session, results) {
-    session.send('update');
+    if(results.station) {
+      session.send('Update: ' + results.station);
+      //TODO: this is where to put the post API call
+    } else {
+      session.send('Sorry, I didn\'t understand that station.');
+    }
   }
 ]);
 
 dialog.matches('Get station status', [
-  function (session, args, next) {
-    console.log(args);
-    next();
-  },
+  searchFor,
   function (session, results) {
-    session.send('get');
+    if(results.station) {
+      session.send('Get: ' + results.station);
+      //TODO: this is where to put the get API call
+    } else {
+      session.send('Sorry, I didn\'t understand that station.');
+    }
   }
 ]);
 
 dialog.onDefault([
   function (session, args, next) {
-    console.log(args)
-    console.log('Sorry, didn\'t understand that!');
+    session.send('Sorry, didn\'t understand that!');
   }
 ]);
+
+function searchFor(session, args, next) {
+  var station = '';
+
+  for (var i = 0; i < args.entities.length; i++) {
+    if(args.entities[i].type == 'Station') {
+      station = args.entities[i].entity;
+      break;
+    }
+  }
+
+  if(station) {
+    index.search(station, function(err, content) {
+      if(content.hits && content.hits.length) {
+        next({ station: content.hits[0].crsCode });
+      }
+    });
+  } else {
+    next();
+  }
+}
